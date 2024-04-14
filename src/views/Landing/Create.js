ec2f44999381b5efcfd50263002fea8f06f55fde
makkahwi <suhaibahmadai@hotmail.com>
2024-04-15 02:04:18 +0300

    Add MinLength To Name Inputs

--- src/views/Landing/Create.js
+++ src/views/Landing/Create.js
@@ -1,7 +1,9 @@
 import { useDispatch } from "react-redux";
-import { setNames } from "values";
-import FormComp from "../../components/Form";
 import { useNavigate } from "react-router-dom";
+
+import { setNames } from "values";
+
+import FormComp from "../../components/Form";
 
 const CreateGame = () => {
   const dispatch = useDispatch();
@@ -12,30 +14,35 @@ const CreateGame = () => {
       name: "master",
       label: "Your Name",
       icon: "fa-solid fa-person-chalkboard",
+      minLength: 4,
       required: true,
     },
     {
       name: "teamAmember1",
       label: "Team A Member 1 Name",
       icon: "fa-solid fa-a",
+      minLength: 4,
       required: true,
     },
     {
       name: "teamAmember2",
       label: "Team A Member 2 Name",
       icon: "fa-solid fa-a",
+      minLength: 4,
       required: true,
     },
     {
       name: "teamBmember1",
       label: "Team B Member 1 Name",
       icon: "fa-solid fa-b",
+      minLength: 4,
       required: true,
     },
     {
       name: "teamBmember2",
       label: "Team B Member 2 Name",
       icon: "fa-solid fa-b",
+      minLength: 4,
       required: true,
     },
   ];
