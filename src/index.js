3074a8d6b237b256447d5592fc46200bd0b0cdc0
makkahwi <suhaibahmadai@hotmail.com>
2024-04-16 18:22:37 +0300

    Fix App Dir & Lang with i18n

--- src/index.js
+++ src/index.js
@@ -1,54 +1,62 @@
-import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import { Container } from "reactstrap";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "assets/scss/argon-design-system.scss?v1.0.0";
 
 import Footer from "components/Footer";
 import Navbar from "components/Navbar";
+import ReactDOM from "react-dom/client";
+import { I18nextProvider, useTranslation } from "react-i18next";
+import { Provider } from "react-redux";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { Container } from "reactstrap";
 import GamePage from "views/Game";
-import RulesPage from "views/Rules";
 import Landing from "views/Landing";
-import i18n from "./i18next";
+import RulesPage from "views/Rules";
 
+import i18next from "./i18next";
 import store from "./redux";
-
-import "@fortawesome/fontawesome-free/css/all.min.css";
-import "assets/scss/argon-design-system.scss?v1.0.0";
-import { I18nextProvider } from "react-i18next";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(
-  <Provider store={store}>
-    <I18nextProvider i18n={i18n}>
-      <BrowserRouter>
-        <Navbar type="transparent" />
+const App = () => {
+  const { i18n } = useTranslation();
 
-        <div className="wrapper">
-          <div
-            className="page-header bg-default"
-            style={{
-              backgroundImage: "url(" + require("assets/img/bg.png") + ")",
-              backgroundSize: "100% auto",
-            }}
-          >
-            <Container>
-              <Routes>
-                {store.getState().game.master ? (
-                  <Route path="/" exact element={<GamePage />} />
-                ) : (
-                  <Route path="/" exact element={<Landing />} />
-                )}
+  document.dir = i18n.dir();
+  document.lang = i18n.language;
 
-                <Route path="/rules" exact element={<RulesPage />} />
-                <Route path="*" element={<Navigate to="/" replace />} />
-              </Routes>
-            </Container>
+  return (
+    <Provider store={store}>
+      <I18nextProvider i18n={i18next}>
+        <BrowserRouter>
+          <Navbar type="transparent" />
+
+          <div className="wrapper">
+            <div
+              className="page-header bg-default"
+              style={{
+                backgroundImage: "url(" + require("assets/img/bg.png") + ")",
+                backgroundSize: "100% auto",
+              }}
+            >
+              <Container>
+                <Routes>
+                  {store.getState().game.master ? (
+                    <Route path="/" exact element={<GamePage />} />
+                  ) : (
+                    <Route path="/" exact element={<Landing />} />
+                  )}
+
+                  <Route path="/rules" exact element={<RulesPage />} />
+                  <Route path="*" element={<Navigate to="/" replace />} />
+                </Routes>
+              </Container>
+            </div>
           </div>
-        </div>
 
-        <Footer />
-      </BrowserRouter>
-    </I18nextProvider>
-  </Provider>
-);
+          <Footer />
+        </BrowserRouter>
+      </I18nextProvider>
+    </Provider>
+  );
+};
+
+root.render(<App />);
