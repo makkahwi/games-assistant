d18339eee3668369ee28eeb5a530126196d85cd8
makkahwi <suhaibahmadai@hotmail.com>
2024-04-18 16:48:46 +0300

    Update ListWords ScoresTable

--- src/views/List/Game/Scores.js
+++ src/views/List/Game/Scores.js
@@ -1,10 +1,20 @@
-import { Table } from "reactstrap";
-import { useSelector } from "react-redux";
 import { useTranslation } from "react-i18next";
+import { useSelector } from "react-redux";
+import { Table } from "reactstrap";
 
 const ScoresTable = () => {
-  const history = useSelector((state) => state.password.history);
+  const members = useSelector((state) => state.list.members);
+  const history = useSelector((state) => state.list.history);
   const { t } = useTranslation();
+
+  const wordCategories = [
+    "Animals",
+    "Colors",
+    "Names",
+    "Places",
+    "Cities",
+    "Countries",
+  ];
 
   return (
     <Table className="align-items-center table-flush" responsive>
@@ -20,39 +30,39 @@ const ScoresTable = () => {
 
       <tbody className="list">
         {history
-          ?.filter(({ team }) => team === "Team A")
-          ?.map(({ team, point, word }, i) => (
+          ?.filter(({ team }) => team === members[0])
+          ?.map(({ team, point, category }, i) => (
             <tr className="text-white" key={i}>
               <td>{t(team)}</td>
-              <td>{word}</td>
+              <td>{wordCategories[category]}</td>
               <td>{point}</td>
             </tr>
           ))}
 
         <tr className="bg-white font-weight-bolder">
-          <td colSpan={2}>{t("Team A Total")}</td>
+          <td colSpan={2}>{t("Member Total", { member: members[0] })}</td>
           <td>
             {history
-              ?.filter(({ team }) => team === "Team A")
+              ?.filter(({ team }) => team === members[0])
               ?.reduce((final, { point }) => final + point, 0)}
           </td>
         </tr>
 
         {history
-          ?.filter(({ team }) => team === "Team B")
-          ?.map(({ team, point, word }, i) => (
+          ?.filter(({ team }) => team === members[1])
+          ?.map(({ team, point, category }, i) => (
             <tr className="text-white" key={i}>
               <td>{t(team)}</td>
-              <td>{word}</td>
+              <td>{wordCategories[category]}</td>
               <td>{point}</td>
             </tr>
           ))}
 
         <tr className="bg-white font-weight-bolder">
-          <td colSpan={2}>{t("Team B Total")}</td>
+          <td colSpan={2}>{t("Member Total", { member: members[0] })}</td>
           <td>
             {history
-              ?.filter(({ team }) => team === "Team B")
+              ?.filter(({ team }) => team === members[1])
               ?.reduce((final, { point }) => final + point, 0)}
           </td>
         </tr>
